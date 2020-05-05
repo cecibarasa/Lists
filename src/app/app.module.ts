85e485784aa529c2b0908ae89c619e02da874fb5
Cecilia Barasa <cecibarasa@gmail.com>
2020-05-05 20:52:21 +0300

    add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { ListDetailComponent } from './list-detail/list-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { ListFormComponent } from './list-form/list-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     ListDetailComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    ListFormComponent
   ],
   imports: [
     BrowserModule,
